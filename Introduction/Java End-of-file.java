5d04664421cb1d2a0e2a4cbb1a3a0e7576cc0049
Muhammet Bilgin <muhammetbilqin@hotmail.com>
2021-05-03 17:29:19 +0300

    HackerRank

---
+++ Introduction/Java End-of-file.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+         Scanner input = new Scanner(System.in);
+        String line;
+        int i = 1;
+        while(input.hasNext())
+        {
+            line = input.nextLine();
+            System.out.println(i + " "+ line);
+            i++;
+        }
+    }
+}
+
+
+//github.com/mhmmtbilgin//
---
+++ Introduction/Welcome to Java!.java
@@ -0,0 +1,7 @@
+
+
+
+System.out.println("Hello, World.");
+System.out.println("Hello, Java.");
+
+//  github.com/mhmmtbilginn //
